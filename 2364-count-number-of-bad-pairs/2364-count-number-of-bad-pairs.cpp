ed411ada7e4c06101ad0a6caf5183f65834b6921
Kushagra Awasthi <kushagraawasthi37@gmail.com>
2026-09-25 04:00:54 +0530

    Time: 67 ms (67.73%), Space: 87.6 MB (75.67%) - LeetHub

--- 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     long long countBadPairs(vector<int>& nums) {
-        vector<int> arr(nums.size(),0);
         unordered_map<int, int> m;
         long long total = 0;
         long long ans = 0;
 
         for (int i = 0; i < nums.size(); i++) {
-            arr[i]=(i - nums[i]);
+            nums[i]=(i - nums[i]);
         }
 
         for(int i =nums.size()-1;i>=0;i--){
-            // cout<<arr[i]<<" ";
-            ans+= (total-m[arr[i]]);
+            ans+= (total-m[nums[i]]);
             total++;
-            m[arr[i]]++;
+            m[nums[i]]++;
         }
 
         return ans;
